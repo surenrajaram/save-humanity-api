ec716ad85fe80e1690ce6946598cbec22ada20d6
Travelling Coder <sg.gajula@gmail.com>
2022-03-12 10:53:46 -0500

    Update index.js

--- save-humanity-api-nodejs/index.js
+++ save-humanity-api-nodejs/index.js
@@ -4,8 +4,11 @@ const PORT = process.env.PORT || 5050;
 
 const { locations } = require('./util/handlers/locations')
 const { addlocation } = require('./util/handlers/addlocation')
+const { getaddress } = require('./util/handlers/getaddress')
+
 app.get('/locations', locations)
 app.get('/addlocation', addlocation);
+app.get('/getaddress', getaddress);
 
 app.get('/get-coordinates', (req, res) => {
     res.send('Save-Humanity API Project');
