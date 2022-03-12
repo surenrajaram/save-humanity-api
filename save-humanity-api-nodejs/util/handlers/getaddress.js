9cb353fd772094936f9e6506a8b7e2c0ea695247
Travelling Coder <sg.gajula@gmail.com>
2022-03-12 10:48:29 -0500

    Update getaddress.js

--- save-humanity-api-nodejs/util/handlers/getaddress.js
+++ save-humanity-api-nodejs/util/handlers/getaddress.js
@@ -1,1 +1,16 @@
-var require = require('pupetteer');
+const puppeteer = require('puppeteer');
+async function getAddress(){
+    const browser = await puppeteer.launch({ headless: true }); // for test disable the headlels mode,
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1000, height: 926 });
+    await page.goto("https://www.google.com/maps/place/28%C2%B010'07.2%22N+82%C2%B018'16.2%22W",{waitUntil: 'networkidle2'});
+
+    /** @type {string[]} */
+    var addressInfo = await page.evaluate(()=>{
+        var address = document.getElementsByClassName('Yr7JMd-pane-hSRGPd')[2].innerHTML;
+        return address
+    })
+
+    console.log(addressInfo)
+    browser.close()
+} 
