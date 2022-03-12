8dc0849739a8d2183f70ffbe31238d6fe5b01b79
Travelling Coder <sg.gajula@gmail.com>
2022-03-12 10:13:56 -0500

    Add files via upload

---
+++ save-humanity-api-nodejs/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 5050;
+
+const { locations } = require('./util/handlers/locations')
+const { addlocation } = require('./util/handlers/addlocation')
+app.get('/locations', locations)
+app.get('/addlocation', addlocation);
+
+app.get('/get-coordinates', (req, res) => {
+    res.send('Save-Humanity API Project');
+});
+
+app.listen(PORT, function() {
+    console.log('Save Humanity Running at: ${PORT}');
+});
+
