2a9cccf9a7305d4e7f87c818fec932b4d5b77cff
surenrajaram <51961671+surenrajaram@users.noreply.github.com>
2022-03-12 13:36:13 -0500

    Update getaddress.js

--- save-humanity-api-nodejs/util/handlers/getaddress.js
+++ save-humanity-api-nodejs/util/handlers/getaddress.js
@@ -1,22 +1,18 @@
-const puppeteer = require('puppeteer');
 const { admin, db } = require("../admin");
+const puppeteer = require('puppeteer');
 
-exports.getAddress = async(req, res) => {    
-    return getAddress();
-}
-
-async function getAddress(){
+exports.getAddress = async(req, res) => {
     const browser = await puppeteer.launch({ headless: true }); // for test disable the headlels mode,
     const page = await browser.newPage();
     await page.setViewport({ width: 1000, height: 926 });
-    await page.goto("https://www.google.com/maps/place/28%C2%B010'07.2%22N+82%C2%B018'16.2%22W",{waitUntil: 'networkidle2'});
-
+    await page.goto("https://www.google.com/maps/place/42.5690651,-83.0948416",{waitUntil: 'networkidle0'});
     /** @type {string[]} */
     var addressInfo = await page.evaluate(()=>{
         var address = document.getElementsByClassName('Yr7JMd-pane-hSRGPd')[2].innerHTML;
         return address;
     });
 
-    // console.log(addressInfo);
+    console.log(addressInfo);
     browser.close();
-} 
+    return res.status(202).json('success');
+}
